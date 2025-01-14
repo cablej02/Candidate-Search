b71596b64d760ba8d72f72b3d5f11615717c4334
Jason Cable <cablej02@gmail.com>
2025-01-14 17:26:07 -0600

    added logic to get each candidate

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -3,37 +3,55 @@ import { searchGithub, searchGithubUser } from '../api/API';
 import { Candidate } from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-    const [candidates, setCandidates] = useState<Candidate[]>([]);
+    const [currentCandidate, setCurrentCandidate] = useState<Candidate | null | undefined>(null);
+    const candidates: Candidate[] = [];
 
     useEffect(() => {
-        const fetchData = async () => {
-            const data = await searchGithub();
-            const newCandidates: Candidate[] = [];
-
-            //loop over each candidate and get their user info to build the new candidate array
-            for (const c of data) {
-                const userInfo = await searchGithubUser(c.login);
-                newCandidates.push({
-                    id: c.id,
-                    name: userInfo.name,
-                    login: c.login,
-                    location: userInfo.location,
-                    avatar_url: c.avatar_url,
-                    email: userInfo.email,
-                    html_url: c.html_url,
-                });
-            }
-
-            console.log('New Candidates:', newCandidates);
-            setCandidates(newCandidates);
-        };
-
-        fetchData();
+        //iife to allow async function
+        (async () => {
+            await fetchCandidates();
+            getNextCandidate();
+        })();
     }, []);
+
+    const fetchCandidates = async () => {
+        const data = await searchGithub();
+
+        //loop over each candidate and get their user info to build the new candidate array
+        for (const c of data) {
+            const userInfo = await searchGithubUser(c.login);
+            candidates.push({
+                id: c.id,
+                name: userInfo.name,
+                login: c.login,
+                location: userInfo.location,
+                avatar_url: c.avatar_url,
+                email: userInfo.email,
+                html_url: c.html_url,
+            });
+        }
+        console.log(candidates);
+    };
+
+    const getNextCandidate = () => {
+        if(candidates.length > 0) {
+            const nextCandidate: Candidate | undefined = candidates.shift();
+            console.log('Next candidate:', nextCandidate);
+            setCurrentCandidate(nextCandidate);
+        } else {
+            console.log('No more candidates');
+            return (
+                <div>
+                    <h1>No more candidates</h1>
+                </div>
+            );
+        }
+    };
 
     return (
         <div>
             <h1>Candidate Search</h1>
+            <p>{currentCandidate?.name ? currentCandidate.name : currentCandidate?.login} {currentCandidate?.login}</p>
         </div>
     );
 };
