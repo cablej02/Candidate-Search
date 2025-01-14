36208d3f1ab32d745ab53e45582ee294c86d5224
Jason Cable <cablej02@gmail.com>
2025-01-14 16:12:09 -0600

    working API call.  gathering all required data for candidates and setting state

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    id: number;
+    name: string;
+    login: string;
+    location: string;
+    avatar_url: string;
+    email: string;
+    html_url: string;
+}
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,12 +1,41 @@
-// import { useState, useEffect } from 'react';
-// import { searchGithub, searchGithubUser } from '../api/API';
+import { useState, useEffect } from 'react';
+import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return (
-    <div>
-      <h1>Candidate Search</h1>
-    </div>
-  );
+    const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await searchGithub();
+            const newCandidates: Candidate[] = [];
+
+            //loop over each candidate and get their user info to build the new candidate array
+            for (const c of data) {
+                const userInfo = await searchGithubUser(c.login);
+                newCandidates.push({
+                    id: c.id,
+                    name: userInfo.name,
+                    login: c.login,
+                    location: userInfo.location,
+                    avatar_url: c.avatar_url,
+                    email: userInfo.email,
+                    html_url: c.html_url,
+                });
+            }
+
+            console.log('New Candidates:', newCandidates);
+            setCandidates(newCandidates);
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Candidate Search</h1>
+        </div>
+    );
 };
 
 export default CandidateSearch;
